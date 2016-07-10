b987da883d83aa3b0e3626e73a32782bc33ae9ce
Ruijuan Li <rzl0007@auburn.edu>
2016-07-10 15:39:30 -0700

    no message

--- data.exploration.R
+++ data.exploration.R
@@ -1,10 +1,15 @@
 setwd('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/')
 
+# load lib for figure
+library(devtools)
+library(rethinking)
+
 # import data 
 list.files()
-firstSNP <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/61822.txt')
-secondSNP <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/18571.txt')
-thirdSNP <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/6224.txt')
+firstSNP <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/61822.txt')
+secondSNP <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/18571.txt')
+thirdSNP <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/raw_SNP_hapmap_data/6224.txt')
+mising_taxa_6224 <- read.delim("~/Desktop/GBS_manuscript/revise/6224_taxa.txt")
 head(firstSNP)
 
 ###################
@@ -14,6 +19,8 @@ head(firstSNP)
 firstSNP.MAF <- firstSNP$Minor.Allele.Frequency
 secondSNP.MAF<- secondSNP$Minor.Allele.Frequency
 thirdSNP.MAF<- thirdSNP$Minor.Allele.Frequency
+density()
+help("density")
 summary(firstSNP.MAF)
 summary(secondSNP.MAF)
 summary(thirdSNP.MAF)
@@ -29,30 +36,34 @@ secondSNP.heterozygosity <- secondSNP$Proportion.Heterozygous
 thirdSNP.heterozygosity <- thirdSNP$Proportion.Heterozygous
 
 # making plot
-hist(firstSNP.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = 'MAF of 61,822 SNPs')
-hist(secondSNP.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = 'MAF of 18,571 SNPs')
-hist(thirdSNP.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = 'MAF of 6,224 SNPs')
+?density
+par(mfrow=c(2,2))
+dens(firstSNP.missingrate, ylim=c(0,20), col="black", xlab="missing rate at locus level", lwd=2, add = F)
+dens(secondSNP.missingrate, xlim=c(0, 1), add = TRUE, col="blue", lwd=2)
+dens(thirdSNP.missingrate, add = TRUE, col="red", lwd=2)
 
-hist(firstSNP.missingrate)
-hist(secondSNP.missingrate)
-hist(thirdSNP.missingrate)
+dens(secondSNP.MAF, col="blue", xlab="Minor allele frequency", lwd=2, ylab=NA)
+dens(firstSNP.MAF, add = TRUE, col="black", lwd=2) 
+dens(thirdSNP.MAF, add = TRUE, col="red", lwd=2)
 
-hist(firstSNP.heterozygosity)
-hist(secondSNP.heterozygosity)
-hist(thirdSNP.heterozygosity)
 
+dens(firstSNP.heterozygosity, col="black", xlab="heterozygosity", add = F, lwd=2)
+dens(secondSNP.heterozygosity, add=TRUE, col="blue", lwd=2)
+dens(thirdSNP.heterozygosity, add = TRUE, col="red", lwd=2)
+
+dens(mising_taxa_6224$Proportion.Missing, col="red", lwd=2, add = F, xlab="missing rate at taxa level", ylab=NA)
 ######################
 # call rate, MAF, & heterozygosity rate for each accession for accessions in each 
 # cluster & sub-cluster
 ######################
 # import data
-SNP_1st_cluster <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/6224_302.txt')
-SNP_2nd_cluster <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/6224_64.txt')
-two_hundred_and_three_accessions <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/203_6430.txt')
-seventy_seven_accessions <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/77_6430.txt')
-ten_accessions <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/10_5283.txt')
-forty_accessions <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/40_5283.txt')
-six_accessions <- read.delim('/Users/ruijuanli/Desktop/revise/MAF_call_rate_heterozygosity/6_5283.txt')
+SNP_1st_cluster <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/6224_302.txt')
+SNP_2nd_cluster <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/64_6224.txt')
+two_hundred_and_three_accessions <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/203_6430.txt')
+seventy_seven_accessions <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/77_6430.txt')
+ten_accessions <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/10_5283.txt')
+forty_accessions <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/40_5283.txt')
+six_accessions <- read.delim('/Users/ruijuanli/Desktop/GBS_manuscript/revise/MAF_call_rate_heterozygosity/6_5283.txt')
 
 ###################
 # call rate, MAF, & heterozygosity rate 
@@ -93,32 +104,38 @@ forty_accessions.heterozygosity <- forty_accessions$Proportion.Heterozygous
 six_accessions.heterozygosity <- six_accessions$Proportion.Heterozygous
 
 # making plot
-hist(SNP_1st_cluster.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = 'MAF of 6224_302 SNPs')
-hist(SNP_2nd_cluster.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = 'MAF of 6224_64 SNPs')
-hist(two_hundred_and_three_accessions.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = '6430_203')
-hist(seventy_seven_accessions.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = '6430_77')
-hist(ten_accessions.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = '10_5283')
-hist(forty_accessions.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = '40_5283')
-hist(six_accessions.MAF, xlab = 'MAF', ylab = 'Number of accessions', main = '6_5283')
+dens(thirdSNP.MAF, col="black", xlab="Minor allele frequency", ylab="density", lwd=2)
+dens(SNP_1st_cluster.MAF, add = TRUE, col="green", lwd=2)
+dens(SNP_2nd_cluster.MAF, add = TRUE, col="red", lwd=2)
 
-hist(SNP_1st_cluster.missingrate)
-hist(SNP_2nd_cluster.missingrate)
-hist(two_hundred_and_three_accessions.missingrate)
-hist(seventy_seven_accessions.missingrate)
-hist(ten_accessions.missingrate)
-hist(forty_accessions.missingrate)
-hist(six_accessions.missingrate)
+# dens(two_hundred_and_three_accessions.MAF, col="blue")
+# dens(seventy_seven_accessions.MAF, add = TRUE, col="pink")
+# dens(ten_accessions.MAF, add = TRUE, col="orange2")
+# dens(forty_accessions.MAF, add = TRUE, col="yellow2")
+# dens(six_accessions.MAF,col="purple4", add = TRUE)
 
-hist(SNP_1st_cluster.heterozygosity)
-hist(SNP_2nd_cluster.heterozygosity)
-hist(two_hundred_and_three_accessions.heterozygosity)
-hist(seventy_seven_accessions.heterozygosity)
-hist(ten_accessions.heterozygosity)
-hist(forty_accessions.heterozygosity)
-hist(six_accessions.heterozygosity)
+dens(thirdSNP.missingrate, ylim=c(0,25), col="black", xlab="missing rate", ylab="density", lwd=2)
+dens(SNP_1st_cluster.missingrate, add = TRUE, col="green", lwd=2)
+dens(SNP_2nd_cluster.missingrate,  col="red", add = T, lwd=2)
+# hist(two_hundred_and_three_accessions.missingrate)
+# hist(seventy_seven_accessions.missingrate)
+# hist(ten_accessions.missingrate)
+# hist(forty_accessions.missingrate)
+# hist(six_accessions.missingrate)
 
+dens(thirdSNP.heterozygosity, col="black", xlab="heterozygosity", ylab="density", lwd=2, ylim=c(0,90))
+dens(SNP_1st_cluster.heterozygosity, add = TRUE, col="green", lwd=2)  
+dens(SNP_2nd_cluster.heterozygosity, col="red", add = T, lwd=2)
+# hist(two_hundred_and_three_accessions.heterozygosity)
+# hist(seventy_seven_accessions.heterozygosity)
+# hist(ten_accessions.heterozygosity)
+# hist(forty_accessions.heterozygosity)
+# hist(six_accessions.heterozygosity)
 
-
+##### missing data at taxa level
+mising_taxa_6224 <- read.delim("~/Desktop/GBS_manuscript/revise/6224_taxa.txt")
+par(mfrow=c(1,2))
+dens(thirdSNP.missingrate, col="red", lwd=2, xlab="missing rate at locus level", ylab=NA)
 
 
 
