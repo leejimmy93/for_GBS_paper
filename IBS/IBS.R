588815ab3c2461cfa0656bbfe9813eb132839a82
Ruijuan Li <rzl0007@auburn.edu>
2016-07-11 15:47:30 -0700

    kinship analysis update

--- IBS/IBS.R
+++ IBS/IBS.R
@@ -31,16 +31,19 @@ dim(kinship.EMMA)
 kinship.EMMA[1:10, 1:10]
 colnames(kinship.EMMA) <- rownames(kinship.EMMA)
 kinship.EMMA[1:10, 1:10]
+write.csv(kinship.EMMA, file = "kinship.EMMA.csv")
 
 # How many pairs are above 0.95 kinship relationship? who are they? they are probably closely related.
 for (i in 1:nrow(kinship.EMMA)){
     for (j in 1:ncol(kinship.EMMA)){
-      if (kinship.EMMA[i,j] >= 0.95 && kinship.EMMA[i,j] != 1){
+      if (kinship.EMMA[i,j] >= 0.91 && kinship.EMMA[i,j] != 1){
         output <- c(rownames(kinship.EMMA)[i], colnames(kinship.EMMA)[j])
         print(output)
       }
     }
 }
+
+kinship.EMMA["PI630019","PI529740"] # 0.92 
 
 # which couple of lines have similarity above 0.98? comment regarding identification of redudant 
 # accessions of germplasm collection... refer to Romay et al. 2013, none!!! 
